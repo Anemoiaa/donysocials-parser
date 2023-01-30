645eb1ce2032b7f6232d9c96c8fe48cfc0a145ce
anemoiaa <georgiypetroff@gmail.com>
2023-01-30 17:21:42 +0400

    feat: add driver options

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ from webdriver_manager.chrome import ChromeDriverManager
 import gspread
 
 from SocialParser import Tiktok, Youtube
-from config import  settings
-pre = {1: Tiktok, 2:Youtube }
+from config import settings
+pre = {1: Tiktok, 2: Youtube}
 
 
 def main() -> None:
     social_media = int(input('Что парсим:\n1.Tiktok\n2.YoutTube\nВвод:'))
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_profile = webdriver.ChromeOptions()
+    chrome_profile.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_profile)
     gc = gspread.service_account(filename=settings.SERVICE_ACCOUNT)
     wrk = gc.open(settings.SHEET_NAME).sheet1
+
     parser = pre[social_media](driver, wrk)
     parser.parse()
 
 
 if __name__ == '__main__':
     main()
-
